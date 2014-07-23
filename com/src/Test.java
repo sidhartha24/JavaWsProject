5b83f772537622a1973883d6126466e471e92df8
sidhartha24 <sidhui.pani@gmail.com>
2014-07-23 11:07:11 +0530

    Update Test.java

--- com/src/Test.java
+++ com/src/Test.java
@@ -5,6 +5,6 @@ public class Test
 	public static void main(String args[])
 	
 	{
-		System.out.println("hello how are you people");
+		System.out.println("hello how are you people have a good time");
 	}
 }
